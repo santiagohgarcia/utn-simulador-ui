fe82a15cdae11afc04cd6f4f038e292745606ed1
shgarcia <shgarcia@tgvamericas.net>
2019-07-05 14:30:14 -0400

    changes

--- src/app/decision-detalle/decision-detalle.component.ts
+++ src/app/decision-detalle/decision-detalle.component.ts
@@ -4,6 +4,28 @@ import { Observable } from 'rxjs/Observable';
 import { ProyectoService } from '../proyecto.service';
 import { ActivatedRoute } from '@angular/router';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+
 @Component({
   selector: 'app-decision-detalle',
   templateUrl: './decision-detalle.component.html',
@@ -11,7 +33,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DecisionDetalleComponent implements OnInit {
 
-  decision$: Observable<any>;
+  decision;
+
+  templateDecision = {
+    descripcion: "",
+    respuestas: []
+  };
 
   saveFunction;
 
@@ -20,19 +47,29 @@ export class DecisionDetalleComponent implements OnInit {
     descripcion: this.descripcion
   });
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
   constructor(private proyectoService: ProyectoService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    var id = this.route.snapshot.paramMap.get('id');
-    this.getDecision(id);
+    var id = Number(this.route.snapshot.paramMap.get('id'));
+    this.getDecision(id).subscribe(d => this.decision = d );
   }
 
   getDecision(id) {
-    if (id) {
-      this.decision$ = this.proyectoService.getDecision(id);
-    }else{
+    return id ? this.proyectoService.getDecision(id) : Observable.of(this.templateDecision);
+  }
 
-    }
+  addRespuesta(){
+    this.decision.respuestas.push({
+      descripcion: "nueva resp"
+    })
+  }
+
+  removeRespuesta(id){
+    var index = this.decision.respuestas.indexOf(this.decision.respuestas.find(r => r.id === id ));
+    this.decision.respuestas.splice(index, 1);
   }
 
 
