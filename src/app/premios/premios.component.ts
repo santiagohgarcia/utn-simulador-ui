2c38145e3476d6cb9738444d677e26372582c802
shgarcia <shgarcia@tgvamericas.net>
2019-11-01 17:03:22 -0400

    premios

--- src/app/premios/premios.component.ts
+++ src/app/premios/premios.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EscenariosService } from '../escenarios.service';
+import { UsuarioService } from '../usuario.service';
+import { switchMap, map, filter } from 'rxjs/operators';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-premios',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremiosComponent implements OnInit {
 
-  constructor() { }
+  usuario;
+  escenarios;
+
+  constructor(private escenariosService: EscenariosService, private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.usuario = this.usuarioService.usuario;
+    this.getEscenarios(this.usuario.id);
   }
 
+  getEscenarios(idUsuario) {
+    const $escenarios = this.escenariosService.getEscenariosParaUsuario(idUsuario);
+
+    const $escenariosConProyectos = $escenarios.pipe(
+      switchMap(escenarios => {
+        const $escenarios = escenarios.map(escenario => {
+          return this.usuarioService.getProyecto(escenario.id, idUsuario).pipe(
+            map(proyecto => {
+              escenario.proyecto = proyecto;
+              return escenario;
+            })
+          )
+        })
+        return zip(...$escenarios);
+      }));
+
+    $escenariosConProyectos.subscribe(escenarios => this.escenarios = escenarios);
+  }
 }
